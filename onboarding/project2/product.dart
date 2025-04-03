10d88eb56ee0226621cd91b35a8fcb725641d2bf
Nathan Rugo <131403788+Nathan-44-rugo@users.noreply.github.com>
2025-04-03 23:26:18 +0300

    Create product.dart

---
+++ onboarding/project2/product.dart
@@ -0,0 +1,31 @@
+class Product {
+  String _name;
+  String _description;
+  double _price;
+
+  Product(this._name, this._description, this._price);
+
+  // Getters
+  String get name => _name;
+  String get description => _description;
+  double get price => _price;
+
+  // Setters
+  set name(String newName) {
+    if (newName.isNotEmpty) _name = newName;
+  }
+
+  set description(String newDescription) {
+    if (newDescription.isNotEmpty) _description = newDescription;
+  }
+
+  set price(double newPrice) {
+    if (newPrice > 0) _price = newPrice;
+  }
+
+  void showDetails() {
+    print("Product Name: $_name");
+    print("Product Description: $_description");
+    print("Product Price: \$${_price.toStringAsFixed(2)}\n");
+  }
+}
